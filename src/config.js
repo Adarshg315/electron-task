b676c9cc7d11dd017bf05d0afc1365e364267b7d
Adarsh Goyal <adarshg315@gmail.com>
2021-03-05 18:46:56 +0530

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-module.exports = {
-	consumer_key: "Tzq2N1yMpWtsgs1kv9ZR16bsh",
-	consumer_secret: "8OOWeF7CwUt4MUf7Nbl5GftERquJkYzWEIFGUQSiKiVVOo8cc7",
-	access_token: "1302304386589229056-YrllZUMMG0YL9AhUYCmTurHWfHV4Nk",
-	access_token_secret: "TrElfjUOBeetTlqk7Ti5bRk92cLYU3FaxCCLlAEjOUfpT",
-};
