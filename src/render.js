ec5152f1a6307767020ef2f92a5e4ce7cb9757bb
Adarshg315 <adarshg315@gmail.com>
2021-03-10 19:29:33 +0530

    fetched latest matches form cricapi

--- src/render.js
+++ src/render.js
@@ -1,6 +1,7 @@
 const config = require("./config");
 var Twit = require("twit");
 const T = new Twit(config);
+const axios = require("axios");
 
 const params = {
 	track: "-is:reply, -is:quote, -is:retweet -is:created",
@@ -13,7 +14,7 @@ const stream = T.stream("statuses/filter", params).on(
 		const tweetStream = document.getElementById("tweetStream");
 		const tweetEl = document.createElement("div");
 		tweetEl.className = "card my-4";
-		tweetEl.innerHTML = `
+		tweetEl.innerHTML = `	
         <div class="card-body">
             <h5 class="card-title">${tweet.text}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${tweet.user.name}</h6>
@@ -26,33 +27,27 @@ const stream = T.stream("statuses/filter", params).on(
 	}
 );
 
-// const axios = require("axios");
+const currentDate = new Date().toISOString();
+const upcoming_match = [];
 
-// const response = fetch(
-// 	"https://cricket-api.vercel.app/cri.php?url=https://m.cricbuzz.com/cricket-commentary/32573/vic-vs-tas-15th-match-sheffield-shield-2020-21"
-// );
+axios
+	.get("https://cricapi.com/api/matches/?apikey=mfqBFEagKSV9l0fCjcC8mNr5HiI2")
+	.then((res) => {
+		res.data.matches.map((element) => {
+			if (element.matchStarted) {
+				if (element.date <= currentDate) {
+					upcoming_match.push(element.unique_id);
+				}
+			}
+		});
 
-// console.log(response);
-// const reader = response.body.getReader();
-
-// while (true) {
-// 	const { value, done } = reader.read();
-// 	if (done) break;
-// 	console.log("Received", value);
-// }
-
-const score = require("cricket-api-node");
-
-score
-	.match(
-		"https://cricket-api.vercel.app/cri.php?url=https://m.cricbuzz.com/cricket-commentary/32573/vic-vs-tas-15th-match-sheffield-shield-2020-21"
-	)
-	.then((live) => {
-		console.log(live);
+		upcoming_match.forEach((id) => {
+			axios
+				.get(
+					`https://cricapi.com/api/cricketScore/?apikey=mfqBFEagKSV9l0fCjcC8mNr5HiI2&unique_id=${id}`
+				)
+				.then((res) => {
+					console.log(res.data.score);
+				});
+		});
 	});
-
-// var cricObj = require("cric-live-json");
-
-// cricObj.getLiveScore(2).then(function (liveScore) {
-// 	console.log("liveScore", liveScore);
-// });
